7be4483e138c4fb0714ae3729dfbd2d983440e1c
suatsunay17 <95652997+suatsunay17@users.noreply.github.com>
2023-03-31 15:41:03 +0300

    added total amount to checkout
    used useState in cartContext to display total amount in checkout page

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -5,23 +5,25 @@ import styles from './Checkout.module.css'
 
 export const Checkout = () => {
 
-    const {items} = useContext(CartContext);
+    const { items,total } = useContext(CartContext);
+    
 
     return (
         <div>
             <h2>Checkout</h2>
+            <h2>Total: ${total.toFixed(2)}</h2>
             <div className='shopping-bag'>
                 {items.map((item) => (
-                     <div className={styles.productItem} key={item.name+Math.random()}>
-            <div className={styles.gallery}>
-                <div className={styles.content}>
-                    <h3>{item.name}</h3>
-                    <img src={item.imageUrl} alt={item.name} />
-                    <h6>${item.price}</h6>
-                </div>
+                    <div className={styles.productItem} key={item.name + Math.random()}>
+                        <div className={styles.gallery}>
+                            <div className={styles.content}>
+                                <h3>{item.name}</h3>
+                                <img src={item.imageUrl} alt={item.name} />
+                                <h6>${item.price}</h6>
+                            </div>
 
-            </div>
-        </div>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,11 +4,13 @@ export const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const addToCart = (name, price,imageUrl) => {
     setItems((prevState) => [...prevState, { name, price,imageUrl }]);
+    setTotal(prevTotal => prevTotal += Number(price))
   };
   return (
-    <CartContext.Provider value={{ items, addToCart}}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ items,total, addToCart}}>{children}</CartContext.Provider>
   );
 }
